8b363592af3ec6dbc8e0b926f7f0fabb0db7cc28
Sovattha Sok <github@soks.fr>
2022-09-23 21:49:41 +0200

    initial

--- index.js
+++ index.js
@@ -1,57 +1,62 @@
-const BAG_SIZE = 50;
-const PLAYERS = 100;
+const Parallel = require("paralleljs");
 
-class Ball {
-    color; // red or blue
-    constructor(color) {
-        this.color = color;
-    }
-}
+const PLAYERS = 1e7;
+(() => {
 
-class Bag {
-    balls = [];
-    constructor() {
-        for (var i = 0; i < BAG_SIZE-1; i++) {
-            this.balls.push(new Ball('red'));
+    function getBlueBall(playerIndex) {
+
+        class Ball {
+            color; // red or blue
+            constructor(color) {
+                this.color = color;
+            }
         }
-        this.balls.push(new Ball('blue'));
-    }
-}
+        
+        class Bag {
+            balls = [];
+            constructor() {
+                for (var i = 0; i < 500-1; i++) {
+                    this.balls.push(new Ball('red'));
+                }
+                this.balls.push(new Ball('blue'));
+            }
+        }
+        
+        class Player {
+            constructor(name) {
+                this.name = name;
+                this.bag = new Bag();
+            }
+            pickBall() {
+                return this.bag.balls[this.randomIntFromInterval(1, this.bag.balls.length-1)];
+            }
+            randomIntFromInterval(min, max) { // min and max included 
+                return Math.floor(Math.random() * (max - min + 1) + min)
+            }
+        }
 
-class Player {
-    constructor(name) {
-        this.name = name;
-        this.bag = new Bag();
-    }
-    pickBall() {
-        return this.bag.balls[this.randomIntFromInterval(1, BAG_SIZE-1)];
-    }
-    randomIntFromInterval(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-}
-
-const resultsMap = new Map();
-(async () => {
-    await Promise.all(Array.from(Array(PLAYERS).keys()).map(playerIndex => getBlueBall(new Player(`player${playerIndex}`)).then(attempt => {
-        resultsMap.set(attempt, (resultsMap.get(attempt) || 0) + 1);
-    })));
-    const sortNumAsc = new Map([...resultsMap].sort((a, b) => a[0] - b[0]));
-    console.log('BAG SIZE', BAG_SIZE);
-    console.log('PLAYERS', PLAYERS);
-    console.log('number of attempts => players count');
-    console.log(sortNumAsc);
-})().then();
-
-
-async function getBlueBall(player) {
-    let attempt = 0;
-    return new Promise(resolve => {
+        const player = new Player(`player${playerIndex}`);
+        let attempt = 0;
         let ballColor = '';
         while(ballColor !== 'blue') {
             ballColor = player.pickBall().color;
             attempt++;
         }
-        resolve(attempt);
+        return (attempt);
+    }
+
+    console.time('Simulation');
+    console.log('BAG SIZE', 500);
+    console.log('PLAYERS', PLAYERS);
+    console.log('number of attempts => players count');
+    const p = new Parallel(Array.from(Array(PLAYERS).keys()));
+    p.map(getBlueBall).then(attempt => {
+        const resultsMap = new Map();
+        attempt.forEach(a => resultsMap.set(a, (resultsMap.get(a) || 0) + 1));
+        const sortNumAsc = new Map([...resultsMap].sort((a, b) => a[0] - b[0]));
+        console.log(sortNumAsc);
+        console.timeEnd('Simulation');
     });
-}
+    
+})();
+
